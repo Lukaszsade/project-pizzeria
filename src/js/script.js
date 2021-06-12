e0a26d7adcbe682e484419f1461676ca2922481a
Paulina Szkudlarek <p.szkudlarek94@gmail.com>
2021-06-12 17:19:42 +0200

    Work with accordion

--- src/js/script.js
+++ src/js/script.js
@@ -59,7 +59,6 @@
       
       for(let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
-                
       } 
     },
 
@@ -106,29 +105,30 @@
       const thisProduct = this;
       console.log('this: ' , this);
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTriggers = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log('clickableTriggers: ', clickableTriggers);
-      
-      for(let clickableTrigger in clickableTriggers) {
-        /* START: add event listener to clickable trigger on event click */
-        clickableTrigger.addEventListener('click', function(event) {
-          /* prevent default action for event */
-          event.preventDefault();
-          console.log('event.preventDefault');
-          /* find active product (product that has active class) */
-          const activeProducts = clickableTrigger.querySelectorAll(select.all.menuProductsActive);
-          console.log('activeProducts: ', activeProducts);
-          for(let activeProduct of activeProducts) {
-            /* if there is active product and it's not thisProduct.element, remove class active from it */
-            if(activeProduct != thisProduct.element) { 
-              activeProduct.classList.remove('active');
-              console.log('usunięto klasę active');
-            }
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger: ', clickableTrigger);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        console.log('event.preventDefault');
+        /* find active product (product that has active class) */
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+        console.log('activeProducts: ', activeProducts);
+        
+        for(let activeProduct of activeProducts) {
+          /* if there is active product and it's not thisProduct.element, remove class active from it */
+          if(activeProduct != thisProduct.element) { 
+            activeProduct.classList.remove('active');
+            console.log('usunięto klasę active');
+          } else {
+            thisProduct.element.classList.add('active');
           }
           /* toggle active class on thisProduct.element */
           thisProduct.element.classList.toggle('active');
-        });
-      }
+        }
+      });
     }
   }
   app.init();
